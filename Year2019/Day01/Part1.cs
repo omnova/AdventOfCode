4703a01f89680a452b174173a17905391c5dee5c
Andrew Schenck <omnova@gmail.com>
2020-12-01 16:47:36 -0600

    Added 2019 Day 1

---
+++ Year2019/Day01/Part1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2019.Day01
+{
+  public class Part1 : IPuzzle
+  {
+    public string Run(string input)
+    {
+      var masses = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+      return masses.Select(m => m / 3 - 2).Sum().ToString();
+    }
+  }
+}
---
+++ Year2019/Day01/Part2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2019.Day01
+{
+  public class Part2 : IPuzzle
+  {
+    public string Run(string input)
+    {
+      var masses = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+      return masses.Select(GetFuelMass).Sum().ToString();
+    }
+
+    private int GetFuelMass(int mass)
+    {
+      int fuelMass = Math.Max(0, mass / 3 - 2);
+
+      return (fuelMass > 0) ? fuelMass + GetFuelMass(fuelMass) : fuelMass;
+    }
+  }
+}
