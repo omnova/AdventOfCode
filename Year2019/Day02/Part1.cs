3643ee971f35a3210d65295e9526e7bdfa89802a
Andrew Schenck <omnova@gmail.com>
2020-12-01 18:00:24 -0600

    Added 2019 Day 2 solution

--- Year2019/Day02/Part1.cs
+++ Year2019/Day02/Part1.cs
@@ -7,7 +7,29 @@ namespace AdventOfCode.Year2019.Day02
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var addresses = input.Split(',').Select(int.Parse).ToList();
+
+      addresses[1] = 12;
+      addresses[2] = 2;
+
+      for (int instructionPointer = 0; instructionPointer < addresses.Count; instructionPointer += 4)
+      {
+        int instruction = addresses[instructionPointer];
+        int operand1 = addresses[addresses[instructionPointer + 1]];
+        int operand2 = addresses[addresses[instructionPointer + 2]];
+        int destinationAddress = addresses[instructionPointer + 3];
+
+        if (instruction == 1)
+          addresses[destinationAddress] = operand1 + operand2;
+        else if (instruction == 2)
+          addresses[destinationAddress] = operand1 * operand2;
+        else if (instruction == 99)
+          break;
+        else
+          return "something went wrong";
+      }
+
+      return addresses[0];
     }
   }
 }
--- Year2019/Day02/Part2.cs
+++ Year2019/Day02/Part2.cs
@@ -8,7 +8,43 @@ namespace AdventOfCode.Year2019.Day02
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var addresses = input.Split(',').Select(int.Parse).ToList();
+
+      for (int noun = 0; noun < 100; noun++)
+      {
+        for (int verb = 0; verb < 100; verb++)
+        {
+          if (GetOutput(addresses, noun, verb) == 19690720)
+            return 100 * noun + verb; // exact OoO
+        }
+      }
+
+      return "wtfmate";
+    }
+
+    private int GetOutput(List<int> inputAddresses, int noun, int verb)
+    {
+      var addresses = inputAddresses.ToList(); // Shallow copy
+
+      addresses[1] = noun;
+      addresses[2] = verb;
+
+      for (int instructionPointer = 0; instructionPointer < addresses.Count; instructionPointer += 4)
+      {
+        int instruction = addresses[instructionPointer];
+        int operand1 = addresses[addresses[instructionPointer + 1]];
+        int operand2 = addresses[addresses[instructionPointer + 2]];
+        int destinationAddress = addresses[instructionPointer + 3];
+
+        if (instruction == 1)
+          addresses[destinationAddress] = operand1 + operand2;
+        else if (instruction == 2)
+          addresses[destinationAddress] = operand1 * operand2;
+        else if (instruction == 99)
+          break;
+      }
+
+      return addresses[0];
     }
   }
 }
