63f767af21747d8dd9be63e8f0549b539a880be8
Andrew Schenck <omnova@gmail.com>
2020-12-02 12:56:20 -0600

    Added overengineering

--- PuzzleRunner.cs
+++ PuzzleRunner.cs
@@ -19,22 +19,96 @@ namespace AdventOfCode
     {
       for (int day = 1; day <= 25; day++)
       {
-        Run(year, day);
+        Run(year, day, 1, true);
+        Run(year, day, 2, true);
       }
     }
 
     public static void Run(int year, int day)
     {
-      Run(year, day, 1);
-      Run(year, day, 2);
+      Run(year, day, 1, false);
+      Run(year, day, 2, false);
     }
 
     public static void Run(int year, int day, int part)
     {
-      Run(year, day, part, GetInputText(year, day));
+      Run(year, day, part, false);
+    }
+
+    private static void Run(int year, int day, int part, bool ignoreNotImplemented)
+    {
+      var puzzle = GetPuzzle(year, day, part);
+
+      if (puzzle == null)
+      {
+        if (!ignoreNotImplemented)
+          Console.WriteLine(OutputFormat, year, day, part, "Solution not implemented");
+
+        return;
+      }
+
+      string input;
+
+      try
+      {
+        input = GetInputFileText(year, day);
+      }
+      catch (FileNotFoundException e)
+      {
+        Console.WriteLine(OutputFormat, year, day, part, "Unable to find input file (" + e.Message + ")");
+        return;
+      }
+
+      try
+      {
+        string output = puzzle.Run(input)?.ToString() ?? "<null>";
+
+        Console.WriteLine(OutputFormat, year, day, part, output);
+      }
+      catch (NotImplementedException)
+      {
+        if (!ignoreNotImplemented)
+          Console.WriteLine(OutputFormat, year, day, part, "Solution not implemented");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(OutputFormat, year, day, part, "Execution failed (" + e.Message + ")");
+      }
     }
 
     public static void Run(int year, int day, int part, string input)
+    {
+      var puzzle = GetPuzzle(year, day, part);
+
+      if (puzzle == null)
+      {
+        Console.WriteLine(OutputFormat, year, day, part, "Unable to find type");
+        return;
+      }
+
+      if (string.IsNullOrEmpty(input))
+      {
+        Console.WriteLine(OutputFormat, year, day, part, "No input provided");
+        return;
+      }
+
+      try
+      {
+        string output = puzzle.Run(input)?.ToString() ?? "<null>";
+
+        Console.WriteLine(OutputFormat, year, day, part, output);
+      }
+      catch (NotImplementedException)
+      {
+        Console.WriteLine(OutputFormat, year, day, part, "Solution not implemented");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(OutputFormat, year, day, part, "Execution failed (" + e.Message + ")");
+      }
+    }
+
+    private static IPuzzle GetPuzzle(int year, int day, int part)
     {
       try
       {
@@ -42,42 +116,24 @@ namespace AdventOfCode
         var type = Assembly.GetExecutingAssembly().GetType(typeName);
 
         if (type == null || type.GetConstructor(new Type[] { }) == null)
-          return;
-        
-        var puzzle = Activator.CreateInstance(type) as IPuzzle;
+          return null;
 
-        if (puzzle == null)
-          return;
-
-        try
-        {
-          string output = puzzle.Run(input).ToString();
-
-          Console.WriteLine(OutputFormat, year, day, part, output);
-        }
-        catch (NotImplementedException)
-        {
-          // Ignore if not implemented.
-        }
-        catch
-        {
-          Console.WriteLine(OutputFormat, year, day, part, "Execution failed (probably bad or missing input)");
-        }
+        return Activator.CreateInstance(type) as IPuzzle;
       }
       catch
       {
-        // Safety net.
+        return null;
       }
     }
 
-    private static string GetInputText(int year, int day)
+    private static string GetInputFileText(int year, int day)
     {
-      string relativePath = string.Format(InputPathFormat, year, day);
+      string inputFilePath = string.Format(InputPathFormat, year, day);
 
-      if (File.Exists(relativePath))
-        return File.ReadAllText(relativePath);
+      if (!File.Exists(inputFilePath))
+        throw new FileNotFoundException(Path.GetFullPath(inputFilePath));
 
-      return string.Empty;
+      return File.ReadAllText(inputFilePath);
     }
   }
 }
