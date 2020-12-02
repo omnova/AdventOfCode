eec7353ec72072eda375890a99fe920f591f1c36
Andrew Schenck <omnova@gmail.com>
2020-12-02 09:01:57 -0600

    Added 2020 Day 2

--- Year2020/Day02/Part1.cs
+++ Year2020/Day02/Part1.cs
@@ -7,7 +7,26 @@ namespace AdventOfCode.Year2020.Day02
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var instructions = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+      int numValidPasswords = 0;
+
+      foreach (var instruction in instructions)
+      {
+        var parts = instruction.Split(new string[] { "-", " ", ": " }, StringSplitOptions.None).ToList();
+
+        int minCount = int.Parse(parts[0]);
+        int maxCount = int.Parse(parts[1]);
+        char ruleChar = parts[2][0];
+        string password = parts[3];
+
+        int charCount = password.Count(c => c == ruleChar);
+
+        if (charCount >= minCount && charCount <= maxCount)
+          numValidPasswords++;
+      }
+
+      return numValidPasswords;
     }
   }
 }
--- Year2020/Day02/Part2.cs
+++ Year2020/Day02/Part2.cs
@@ -7,7 +7,24 @@ namespace AdventOfCode.Year2020.Day02
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var instructions = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+      int numValidPasswords = 0;
+
+      foreach (var instruction in instructions)
+      {
+        var parts = instruction.Split(new string[] { "-", " ", ": " }, StringSplitOptions.None).ToList();
+
+        int position1 = int.Parse(parts[0]) - 1;
+        int position2 = int.Parse(parts[1]) - 1;
+        char ruleChar = parts[2][0];
+        string password = parts[3];
+
+        if ((password[position1] == ruleChar && password[position2] != ruleChar) || (password[position1] != ruleChar && password[position2] == ruleChar))
+          numValidPasswords++;
+      }
+
+      return numValidPasswords;
     }
   }
 }
