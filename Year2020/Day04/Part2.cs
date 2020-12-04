993b66a6ff7a40b65f8cf7ae6f3d41936c02a227
Andrew Schenck <omnova@gmail.com>
2020-12-04 12:36:55 -0600

    Regex-only validations and greatly simplified solution

--- Year2020/Day04/Part2.cs
+++ Year2020/Day04/Part2.cs
@@ -9,15 +9,15 @@ namespace AdventOfCode.Year2020.Day04
   {
     public object Run(string input)
     {
-      var fieldValidations = new Dictionary<string, Func<string, bool>>
+      var fieldValidations = new Dictionary<string, string>
       {
-        { "byr", IsValidBirthYear },
-        { "iyr", IsValidIssueDate },
-        { "eyr", IsValidExpirationYear },
-        { "hgt", IsValidHeight },
-        { "hcl", IsValidHairColor },
-        { "ecl", IsValidEyeColor },
-        { "pid", IsValidPassportID }
+        { "byr", @"^19[2-9]\d$|^200[0-2]$" },
+        { "iyr", @"^201\d$|^2020$" },
+        { "eyr", @"^202\d$|^2030$" },
+        { "hgt", @"^((1[5-8]\d|19[0-3])cm|(59|6\d|7[0-6])in)$" },
+        { "hcl", @"^#[0-9a-f]{6}$" },
+        { "ecl", @"^(amb|blu|brn|gry|grn|hzl|oth)$" },
+        { "pid", @"^\d{9}$" }
       };
 
       var passports = input.Split(Environment.NewLine + Environment.NewLine);
@@ -28,75 +28,13 @@ namespace AdventOfCode.Year2020.Day04
       {
         var fields = passport.Split(new string[] { " ", Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).Select(f => f.Split(':')).ToList();
 
-        bool isValid = true;
-
-        foreach (var validation in fieldValidations)
-        {
-          var foundField = fields.Find(f => f[0] == validation.Key);
-            
-          if (foundField == null || !validation.Value(foundField[1]))
-          {
-            isValid = false;
-            break;
-          }
-        }
+        bool isValid = fieldValidations.All(v => fields.Any(f => v.Key == f[0] && Regex.IsMatch(f[1], v.Value)));
 
         if (isValid)
           numValidPassports++;
       }
 
       return numValidPassports;
-    }
-
-    private bool IsValidBirthYear(string value)
-    {
-      return Regex.IsMatch(value, @"^\d{4}$") && int.Parse(value) >= 1920 && int.Parse(value) <= 2002;
-    }
-
-    private bool IsValidIssueDate(string value)
-    {
-      return Regex.IsMatch(value, @"^\d{4}$") && int.Parse(value) >= 2010 && int.Parse(value) <= 2020;
-    }
-
-    private bool IsValidExpirationYear(string value)
-    {
-      return Regex.IsMatch(value, @"^\d{4}$") && int.Parse(value) >= 2020 && int.Parse(value) <= 2030;
-    }
-
-    private bool IsValidHeight(string value)
-    {
-      if (Regex.IsMatch(value, @"^\d{3}cm$"))
-      {
-        int height = int.Parse(value.Substring(0, 3));
-
-        return height >= 150 && height <= 193;
-      }
-      
-      if (Regex.IsMatch(value, @"^\d{2}in$"))
-      {
-        int height = int.Parse(value.Substring(0, 2));
-
-        return height >= 59 && height <= 76;
-      }
-
-      return false;
-    }
-
-    private bool IsValidHairColor(string value)
-    {
-      return Regex.IsMatch(value, @"^#[0-9a-f]{6}$");
-    }
-
-    private bool IsValidEyeColor(string value)
-    {
-      string[] validColors = new string[] { "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
-
-      return validColors.Contains(value);
-    }
-
-    private bool IsValidPassportID(string value)
-    {
-      return Regex.IsMatch(value, @"^\d{9}$");
     }
   }
 }
