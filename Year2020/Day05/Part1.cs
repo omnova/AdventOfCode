eb5ed618ea2235fd13db8ed4d811a2828a70c5ec
Andrew Schenck <omnova@gmail.com>
2020-12-05 02:52:54 -0600

    Added 2020 Day 5

--- Year2020/Day05/Part1.cs
+++ Year2020/Day05/Part1.cs
@@ -7,7 +7,41 @@ namespace AdventOfCode.Year2020.Day05
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var seats = input.Split(Environment.NewLine);
+
+      int maxSeatId = seats.Max(GetSeatId);
+
+      return maxSeatId;
+    }
+
+    private int GetSeatId(string seat)
+    {
+      string rowInstructions = seat.Substring(0, 7);
+      string columnInstructions = seat.Substring(7);
+
+      int row = 0;
+      int rowIncrement = 64;
+
+      foreach (char instruction in rowInstructions.ToCharArray())
+      {
+        if (instruction == 'B')
+          row += rowIncrement;
+
+        rowIncrement /= 2;
+      }
+
+      int column = 0;
+      int columnIncrement = 4;
+
+      foreach (char instruction in columnInstructions.ToCharArray())
+      {
+        if (instruction == 'R')
+          column += columnIncrement;
+
+        columnIncrement /= 2;
+      }
+
+      return (row * 8) + column;
     }
   }
 }
--- Year2020/Day05/Part2.cs
+++ Year2020/Day05/Part2.cs
@@ -8,7 +8,39 @@ namespace AdventOfCode.Year2020.Day05
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var seatInstructions = input.Split(Environment.NewLine);
+
+      var seats = new int[128 * 8];
+
+      seatInstructions.Select(s => GetSeatId(s)).ToList().ForEach(s => seats[s] = s);
+
+      int seatId = seats.Skip(8).Take(126 * 8).ToList().IndexOf(0) + 8;
+
+      return seatId;
+    }
+
+    private int GetSeatId(string seatInstruction)
+    {
+      string rowInstructions = seatInstruction.Substring(0, 7);
+      string columnInstructions = seatInstruction.Substring(7);
+
+      int row = 0;
+
+      for (int i = 0; i < 7; i++)
+      {
+        if (rowInstructions[i] == 'B')
+          row += (1 << (6 - i));
+      }
+
+      int column = 0;
+
+      for (int i = 0; i < 3; i++)
+      {
+        if (columnInstructions[i] == 'R')
+          column += (1 << (2 - i));
+      }
+     
+      return (row * 8) + column;
     }
   }
 }
