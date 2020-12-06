e806020a295feb69ec8fa76986a8595160e25f41
Andrew Schenck <omnova@gmail.com>
2020-12-05 23:44:13 -0600

    Added 2020 Day 6

--- Year2020/Day06/Part1.cs
+++ Year2020/Day06/Part1.cs
@@ -8,7 +8,21 @@ namespace AdventOfCode.Year2020.Day06
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var groups = input.Split(Environment.NewLine + Environment.NewLine);
+
+      int totalYesCounts = 0;
+
+      foreach (var group in groups)
+      {
+        var groupAnswers = new HashSet<char>();
+        var people = group.Split(Environment.NewLine).ToList();
+
+        people.ForEach(person => person.ToCharArray().ToList().ForEach(y => groupAnswers.Add(y)));
+
+        totalYesCounts += groupAnswers.Count();
+      }
+
+      return totalYesCounts;
     }
   }
 }
--- Year2020/Day06/Part2.cs
+++ Year2020/Day06/Part2.cs
@@ -8,7 +8,29 @@ namespace AdventOfCode.Year2020.Day06
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var groups = input.Split(Environment.NewLine + Environment.NewLine);
+
+      int totalYesCounts = 0;
+
+      foreach (var group in groups)
+      {
+        var groupAnswers = new HashSet<char>("abcdefghijklmnopqrstuvwxyz".ToCharArray());
+
+        var people = group.Split(Environment.NewLine);
+
+        foreach (var person in people)
+        {
+          var personAnswers = new HashSet<char>();
+
+          person.ToCharArray().ToList().ForEach(y => personAnswers.Add(y));
+
+          groupAnswers.IntersectWith(personAnswers);
+        }
+
+        totalYesCounts += groupAnswers.Count;
+      }
+
+      return totalYesCounts;
     }
   }
 }
