74aeee59d38192447aca3fe8c4dce66fca26f8f1
Andrew Schenck <omnova@gmail.com>
2020-12-07 00:01:41 -0600

    Added 2020 Day 7

--- Year2020/Day07/Part1.cs
+++ Year2020/Day07/Part1.cs
@@ -8,7 +8,54 @@ namespace AdventOfCode.Year2020.Day07
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var lines = input.Split("." + Environment.NewLine);
+
+      var relationships = new Dictionary<string, Dictionary<string, int>>();
+
+      foreach (var line in lines)
+      {
+        var parts = line.Split(" contain ");
+
+        string container = parts[0];
+
+        relationships.TryAdd(container, new Dictionary<string, int>());
+
+        if (parts[1] == "no other bags")
+          continue;
+
+        foreach (var contains in parts[1].Split(", "))
+        {
+          string contained = contains.Substring(contains.IndexOf(' ') + 1);
+
+          if (contained.Last() == 'g')
+            contained += "s";
+
+          int number = int.Parse(contains.Substring(0, contains.IndexOf(' ')));
+
+          if (!relationships[container].TryAdd(contained, number))
+            relationships[container][contained] += number;
+
+          if (!relationships.ContainsKey(contained))
+            relationships.Add(contained, new Dictionary<string, int>());
+        }
+      }
+
+      int numGoldContainers = relationships.Count(r => ContainsGold(relationships, r.Key));
+
+      return numGoldContainers;
+    }
+
+    private bool ContainsGold(Dictionary<string, Dictionary<string, int>> relationships, string container)
+    {
+      foreach (var contained in relationships[container])
+      {
+        if (contained.Key == "shiny gold bags")
+          return true;
+        else if (ContainsGold(relationships, contained.Key))
+          return true;
+      }
+
+      return false;
     }
   }
 }
--- Year2020/Day07/Part2.cs
+++ Year2020/Day07/Part2.cs
@@ -8,7 +8,53 @@ namespace AdventOfCode.Year2020.Day07
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var lines = input.Split("." + Environment.NewLine);
+
+      var relationships = new Dictionary<string, Dictionary<string, int>>();
+
+      foreach (var line in lines)
+      {
+        var parts = line.Split(" contain ");
+
+        string container = parts[0];
+
+        relationships.TryAdd(container, new Dictionary<string, int>());
+
+        if (parts[1] == "no other bags")
+          continue;
+
+        foreach (var contains in parts[1].Split(", "))
+        {
+          string contained = contains.Substring(contains.IndexOf(' ') + 1);
+
+          if (contained.Last() == 'g')
+            contained += "s";
+
+          int number = int.Parse(contains.Substring(0, contains.IndexOf(' ')));
+
+          if (!relationships[container].TryAdd(contained, number))
+            relationships[container][contained] += number;
+
+          if (!relationships.ContainsKey(contained))
+            relationships.Add(contained, new Dictionary<string, int>());
+        }
+      }
+
+      int numGoldContainers = GetContainedBagCount(relationships, "shiny gold bags");
+
+      return numGoldContainers;
+    }
+
+    private int GetContainedBagCount(Dictionary<string, Dictionary<string, int>> relationships, string container)
+    {
+      int numContainedBags = 0;
+
+      foreach (var contained in relationships[container])
+      {
+        numContainedBags += GetContainedBagCount(relationships, contained.Key) * contained.Value + contained.Value;
+      }
+
+      return numContainedBags;
     }
   }
 }
