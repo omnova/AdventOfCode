d7cc698eed4a1118725436f36c0b7e99661b9870
Andrew Schenck <omnova@gmail.com>
2020-12-08 10:50:16 -0600

    Added 2020 Day 8

--- Year2020/Day08/Part1.cs
+++ Year2020/Day08/Part1.cs
@@ -8,7 +8,33 @@ namespace AdventOfCode.Year2020.Day08
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var instructions = input.Split(Environment.NewLine).Select(i => i.Split(" ")).Select(i => new { Op = i[0], Arg = int.Parse(i[1]) }).ToList();
+      var runInstructions = new List<int>();
+
+      int accumulator = 0;
+      int i = 0;
+
+      while (true)
+      {
+        if (runInstructions.Contains(i))
+          break;
+        else
+          runInstructions.Add(i);
+
+        var instruction = instructions[i];
+
+        if (instruction.Op == "jmp")
+          i += (instruction.Arg % instructions.Count);
+        else
+        {
+          if (instruction.Op == "acc")
+            accumulator += instruction.Arg;
+
+          i = (i + 1) % instructions.Count;
+        }
+      }
+
+      return accumulator;
     }
   }
 }
--- Year2020/Day08/Part2.cs
+++ Year2020/Day08/Part2.cs
@@ -6,9 +6,54 @@ namespace AdventOfCode.Year2020.Day08
 {
   public class Part2 : IPuzzle
   {
+    private class Instruction
+    {
+      public string Op;
+      public int Arg;
+    }
+
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var originalInstructions = input.Split(Environment.NewLine).Select(i => i.Split(" ")).Select(i => new Instruction { Op = i[0], Arg = int.Parse(i[1]) }).ToList();      
+
+      for (int attempt = 0; attempt < originalInstructions.Count; attempt++)
+      {
+        if (!(originalInstructions[attempt].Op == "nop" || originalInstructions[attempt].Op == "jmp"))
+          continue;
+
+        var instructions = originalInstructions.Select(i => new Instruction { Op = i.Op, Arg = i.Arg }).ToList();
+        var runInstructions = new List<int>();
+
+        instructions[attempt].Op = instructions[attempt].Op == "jmp" ? "nop" : "jmp";
+
+        int accumulator = 0;
+        int i = 0;
+
+        while (true)
+        {
+          if (i == instructions.Count)
+            return accumulator;
+
+          if (runInstructions.Contains(i))
+            break;
+          else
+            runInstructions.Add(i);
+
+          var instruction = instructions[i];
+
+          if (instruction.Op == "jmp")
+            i += (instruction.Arg % instructions.Count);
+          else
+          {
+            if (instruction.Op == "acc")
+              accumulator += instruction.Arg;
+
+            i = (i + 1) % instructions.Count;
+          }
+        }
+      }
+
+      return null;
     }
   }
 }
