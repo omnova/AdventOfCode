7cab334c497c57ffcadda210315395c284b71a9d
Andrew Schenck <omnova@gmail.com>
2020-12-12 12:28:42 -0600

    Added 2020 Day 10

--- Year2020/Day10/Part1.cs
+++ Year2020/Day10/Part1.cs
@@ -8,7 +8,20 @@ namespace AdventOfCode.Year2020.Day10
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var adapters = input.Split(Environment.NewLine).Select(int.Parse).OrderBy(a => a).ToList();
+
+      int num1Diff = 0 + (adapters[0] == 1 ? 1 : 0);
+      int num3Diff = 1 + (adapters[0] == 3 ? 1 : 0);
+
+      for (int i = 0; i < adapters.Count - 1; i++)
+      {
+        if (adapters[i + 1] - adapters[i] == 1)
+          num1Diff++;
+        else if (adapters[i + 1] - adapters[i] == 3)
+          num3Diff++;
+      }
+
+      return num1Diff * num3Diff;
     }
   }
 }
