16e370a83a63569c1acfdd41dd897d9316d82519
Andrew Schenck <omnova@gmail.com>
2020-12-12 12:23:12 -0600

    Added 2020 Day 12

--- Year2020/Day12/Part1.cs
+++ Year2020/Day12/Part1.cs
@@ -8,7 +8,44 @@ namespace AdventOfCode.Year2020.Day12
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var instructions = input.Split(Environment.NewLine).Select(i => new { Op = i[0], Value = int.Parse(i.Substring(1)) }).ToList();
+
+      int x = 0;
+      int y = 0;
+      int angle = 90;
+
+      foreach (var instruction in instructions)
+      {
+        if (instruction.Op == 'N')
+          y -= instruction.Value;
+        else if (instruction.Op == 'S')
+          y += instruction.Value;
+        else if (instruction.Op == 'W')
+          x -= instruction.Value;
+        else if (instruction.Op == 'E')
+          x += instruction.Value;
+        else if (instruction.Op == 'L')
+          angle = (360 + angle - instruction.Value) % 360;
+        else if (instruction.Op == 'R')
+          angle = (angle + instruction.Value) % 360;
+        else if (instruction.Op == 'F')
+        {
+          if (angle == 0)
+            y -= instruction.Value;
+          else if (angle == 90)
+            x += instruction.Value;
+          else if (angle == 180)
+            y += instruction.Value;
+          else if (angle == 270)
+            x -= instruction.Value;
+          else
+            return null;
+        }
+        else
+          return null;
+      }
+
+      return Math.Abs(x) + Math.Abs(y);
     }
   }
 }
--- Year2020/Day12/Part2.cs
+++ Year2020/Day12/Part2.cs
@@ -8,7 +8,53 @@ namespace AdventOfCode.Year2020.Day12
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var instructions = input.Split(Environment.NewLine).Select(i => new { Op = i[0], Value = int.Parse(i.Substring(1)) }).ToList();
+
+      int shipX = 0;
+      int shipY = 0;
+      int waypointX = 10;
+      int waypointY = -1;
+
+      foreach (var instruction in instructions)
+      {
+        if (instruction.Op == 'N')
+          waypointY -= instruction.Value;
+        else if (instruction.Op == 'S')
+          waypointY += instruction.Value;
+        else if (instruction.Op == 'W')
+          waypointX -= instruction.Value;
+        else if (instruction.Op == 'E')
+          waypointX += instruction.Value;
+        else if (instruction.Op == 'L')
+        {
+          for (int R = 90; R <= instruction.Value; R += 90)
+          {
+            int tempX = waypointX;
+            int tempY = waypointY;
+
+            waypointX = tempY;
+            waypointY = -tempX;
+          }
+        }
+        else if (instruction.Op == 'R')
+        {
+          for (int R = 90; R <= instruction.Value; R += 90)
+          {
+            int tempX = waypointX;
+            int tempY = waypointY;
+
+            waypointX = -tempY;
+            waypointY = tempX;
+          }
+        }
+        else if (instruction.Op == 'F')
+        {
+          shipX += (waypointX * instruction.Value);
+          shipY += (waypointY * instruction.Value);
+        }
+      }
+
+      return Math.Abs(shipX) + Math.Abs(shipY);
     }
   }
 }
