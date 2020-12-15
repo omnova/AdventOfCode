55201e0e36d76d5c3aefee1c84bf22b44a25f19d
Andrew Schenck <omnova@gmail.com>
2020-12-15 01:15:56 -0600

    Added 2020 Day 15

--- Year2020/Day15/Part1.cs
+++ Year2020/Day15/Part1.cs
@@ -8,7 +8,39 @@ namespace AdventOfCode.Year2020.Day15
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var numbers = input.Split(',').Select(int.Parse).ToList();
+
+      var lastUsages = new Dictionary<int, int>();
+      int lastNumber = 0;
+      bool isLastNumberNew = true;
+
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        lastUsages.Add(numbers[i], i);
+        lastNumber = numbers[i];
+      }
+
+      for (int i = numbers.Count; i < 2020; i++)
+      {
+        int nextNumber = 0;
+
+        // Last turn
+        if (isLastNumberNew)
+        {
+          nextNumber = 0;
+          lastUsages[lastNumber] = i - 1;
+        }
+        else
+        {
+          nextNumber = i - 1 - lastUsages[lastNumber];
+          lastUsages[lastNumber] = i - 1;
+        }
+
+        isLastNumberNew = !lastUsages.ContainsKey(nextNumber);
+        lastNumber = nextNumber;
+      }
+
+      return lastNumber;
     }
   }
 }
