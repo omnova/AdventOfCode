8adfad1cb7cd9ad9bea43381b9846e85550f0afc
Andrew Schenck <omnova@gmail.com>
2020-12-15 14:07:39 -0600

    Added 2020 Day 13

--- Year2020/Day13/Part1.cs
+++ Year2020/Day13/Part1.cs
@@ -8,7 +8,24 @@ namespace AdventOfCode.Year2020.Day13
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      long departureTime = int.Parse(input.Split(Environment.NewLine)[0]);
+      var busses = input.Split(Environment.NewLine)[1].Split(",").Where(b => b != "x").Select(int.Parse).ToDictionary(b => b, b => (long)0);
+
+      for (long t = 0; t < 10000000; t++)
+      {
+        foreach (var bus in busses)
+        {
+          if (busses[bus.Key] + bus.Key == t)
+          {
+            busses[bus.Key] += bus.Key;
+
+            if (t >= departureTime)
+              return bus.Key * (t - departureTime);
+          }
+        } 
+      }
+
+      return null;
     }
   }
 }
