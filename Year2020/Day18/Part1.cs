b000b4769fb5fab945b599a5e054010edd6588a7
Andrew Schenck <omnova@gmail.com>
2020-12-19 16:58:20 -0600

    Fuck you math

--- Year2020/Day18/Part1.cs
+++ Year2020/Day18/Part1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Year2020.Day18
 {
@@ -6,7 +8,44 @@ namespace AdventOfCode.Year2020.Day18
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      long result = input.Replace(" ", "").Split(Environment.NewLine).Select(l => DoMaths(l, 0, out int length)).Sum();
+
+      return result;
+    }
+
+    private long DoMaths(string math, int offset, out int newOffset)
+    {
+      long result = 0;
+      int i = offset;
+      Func<long, long, long> operation = null;
+
+      for (; i < math.Length && math[i] != ')'; i++)
+      {
+        long operand = 0;
+
+        if (math[i] == '(')
+        {
+          operand = DoMaths(math, ++i, out i);
+        }
+        else if (char.IsDigit(math[i]))
+          operand = int.Parse(math[i].ToString());
+        
+        if (math[i] == '+')
+          operation = new Func<long, long, long>((o1, o2) => o1 + o2);
+        else if (math[i] == '*')
+          operation = new Func<long, long, long>((o1, o2) => o1 * o2);
+        else if (operation == null)
+          result = operand;
+        else
+        {
+          result = operation(result, operand);
+          operation = null;
+        }
+      }
+
+      newOffset = i;
+
+      return result;
     }
   }
 }
--- Year2020/Day18/Part2.cs
+++ Year2020/Day18/Part2.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode.Year2020.Day18
 {
@@ -6,7 +9,53 @@ namespace AdventOfCode.Year2020.Day18
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      long result = input.Replace(" ", "").Split(Environment.NewLine).Select(DoMaths).Sum();
+
+      return result;
+    }
+
+    private long DoMaths(string math)
+    {      
+      for (var match = Regex.Match(math, @"\([^\(\)]+\)"); match.Success; match = Regex.Match(math, @"\([^\(\)]+\)"))
+      {
+        string matchValue = match.Value;
+
+        DoAddition(ref matchValue);
+        DoMultiplication(ref matchValue);
+        DoParentheses(ref matchValue);
+
+        math = math.Replace(match.Value, matchValue);
+      }
+
+      DoAddition(ref math);
+      DoMultiplication(ref math);
+
+      return long.Parse(math);
+    }
+
+    private void DoParentheses(ref string math)
+    {
+      math = math.Substring(1, math.Length - 2);
+    }
+
+    private void DoAddition(ref string math)
+    {
+      for (var match = Regex.Match(math, @"\d+\+\d+"); match.Success; match = Regex.Match(math, @"\d+\+\d+"))
+      {      
+        long result = match.Value.Split("+").Select(long.Parse).Sum();
+
+        math = new Regex(match.Value.Replace("+", @"\+")).Replace(math, result.ToString(), 1);
+      }
+    }
+
+    private void DoMultiplication(ref string math)
+    {
+      for (var match = Regex.Match(math, @"\d+\*\d+"); match.Success; match = Regex.Match(math, @"\d+\*\d+"))
+      {
+        long result = match.Value.Split("*").Select(long.Parse).Aggregate(1L, (acc, val) => acc * val);
+
+        math = new Regex(match.Value.Replace("*", @"\*")).Replace(math, result.ToString(), 1);
+      }
     }
   }
 }
