58751d1aca77e9c2624f1fa75623c5b56b2c8df1
Andrew Schenck <omnova@gmail.com>
2020-12-20 17:51:32 -0600

    Added 2020 Day 20

--- Year2020/Day20/Part1.cs
+++ Year2020/Day20/Part1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Year2020.Day20
 {
@@ -6,7 +8,107 @@ namespace AdventOfCode.Year2020.Day20
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var tiles = input.Replace("Tile ", "").Replace(":", "").Split(Environment.NewLine + Environment.NewLine).Select(t => new Tile(t)).ToList();
+
+      var grid = new List<List<Tile>> { new List<Tile> { tiles[0] } };
+      var initialRow = grid[0];      
+
+      var matchedTiles = new List<Tile>();
+      tiles.RemoveAt(0);
+
+
+      // Build out first row
+      do
+      {
+        matchedTiles.Clear();
+
+        foreach (var tile in tiles)
+        {
+          if (initialRow[initialRow.Count - 1].IsMatchRight(tile))
+          {
+            initialRow.Add(tile);
+            matchedTiles.Add(tile);
+          }
+          else if (initialRow[0].IsMatchLeft(tile))
+          {
+            initialRow.Insert(0, tile);
+            matchedTiles.Add(tile);
+          }
+        }
+
+        tiles.RemoveAll(t => matchedTiles.Contains(t));
+      }
+      while (matchedTiles.Any());
+
+
+      // Build out first column
+      do
+      {
+        matchedTiles.Clear();
+
+        foreach (var tile in tiles)
+        {
+          if (grid[0][0].IsMatchTop(tile))
+          {
+            grid.Insert(0, Enumerable.Repeat(default(Tile), grid[0].Count).ToList());
+            grid[0][0] = tile;
+            matchedTiles.Add(tile);
+          }
+          else if (grid[grid.Count - 1][0].IsMatchBottom(tile))
+          {
+            grid.Add(Enumerable.Repeat(default(Tile), grid[0].Count).ToList());
+            grid[grid.Count - 1][0] = tile;
+            matchedTiles.Add(tile);
+          }
+        }
+
+        tiles.RemoveAll(t => matchedTiles.Contains(t));
+      }
+      while (matchedTiles.Any());
+
+
+      // Build out the rest
+      for (int y = grid.IndexOf(initialRow); y > 0; y--)
+      {
+        for (int x = 1; x < grid[0].Count; x++)
+        {
+          matchedTiles.Clear();
+
+          foreach (var tile in tiles)
+          {
+            if (grid[y][x].IsMatchTop(tile))
+            {
+              grid[y - 1][x] = tile;
+              matchedTiles.Add(tile);
+              break;
+            }
+          }
+
+          tiles.RemoveAll(t => matchedTiles.Contains(t));
+        }
+      }
+
+      for (int y = grid.IndexOf(initialRow); y < grid.Count - 1; y++)
+      {
+        for (int x = 1; x < grid[0].Count; x++)
+        {
+          matchedTiles.Clear();
+
+          foreach (var tile in tiles)
+          {
+            if (grid[y][x].IsMatchBottom(tile))
+            {
+              grid[y + 1][x] = tile;
+              matchedTiles.Add(tile);
+              break;
+            }
+          }
+
+          tiles.RemoveAll(t => matchedTiles.Contains(t));
+        }
+      }
+
+      return grid[0][0].Id * grid[0][grid[0].Count - 1].Id * grid[grid.Count - 1][0].Id * grid[grid.Count - 1][grid[0].Count - 1].Id;
     }
   }
 }
---
+++ Year2020/Day20/Tile.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2020.Day20
+{
+  internal class Tile
+  {
+    private int orientation = 0;
+    private List<bool[,]> cachedOrientations = new List<bool[,]>();
+    private int size = 0;
+
+    public long Id { get; private set; }
+    public bool[,] Data => this.cachedOrientations[this.orientation];
+
+    public List<bool[,]> Orientations => this.cachedOrientations;
+
+    public Tile(string data)
+    {
+      var lines = data.Split(Environment.NewLine);
+
+      this.Id = int.Parse(lines[0]);
+      this.size = lines[1].Length;
+
+      var initialOrientation = new bool[this.size, this.size];
+
+      for (int y = 0; y < this.size; y++)
+      {
+        for (int x = 0; x < this.size; x++)
+          initialOrientation[x, y] = lines[y + 1][x] == '#';
+      }
+
+      BuildOrientations(initialOrientation);
+    }
+
+    public bool IsMatchRight(Tile tile, bool allowReorientation = true)
+    {
+      for (int orientation = 0; orientation < this.Orientations.Count; orientation++)
+      {
+        bool isMatch = true;
+
+        for (int i = 0; i < this.size; i++)
+        {
+          if (this.Data[this.size - 1, i] != tile.Data[0, i])
+          {
+            isMatch = false;
+            break;
+          }
+        }
+
+        if (isMatch)
+          return true;
+
+        if (!allowReorientation)
+          break;
+
+        if (orientation < this.Orientations.Count - 1)
+          tile.NextOrientation();
+      }
+
+      return false;
+    }
+
+    public bool IsMatchLeft(Tile tile, bool allowReorientation = true)
+    {
+      for (int orientation = 0; orientation < this.Orientations.Count; orientation++)
+      {
+        bool isMatch = true;
+
+        for (int i = 0; i < this.size; i++)
+        {
+          if (this.Data[0, i] != tile.Data[this.size - 1, i])
+          {
+            isMatch = false;
+            break;
+          }
+        }
+
+        if (isMatch)
+          return true;
+
+        if (!allowReorientation)
+          break;
+
+        if (orientation < this.Orientations.Count - 1)
+          tile.NextOrientation();
+      }
+
+      return false;
+    }
+
+    public bool IsMatchTop(Tile tile, bool allowReorientation = true)
+    {
+      for (int orientation = 0; orientation < this.Orientations.Count; orientation++)
+      {
+        bool isMatch = true;
+
+        for (int i = 0; i < this.size; i++)
+        {
+          if (this.Data[i, 0] != tile.Data[i, this.size - 1])
+          {
+            isMatch = false;
+            break;
+          }
+        }
+
+        if (isMatch)
+          return true;
+
+        if (!allowReorientation)
+          break;
+
+        if (orientation < this.Orientations.Count - 1)
+          tile.NextOrientation();
+      }
+
+      return false;
+    }
+
+    public bool IsMatchBottom(Tile tile, bool allowReorientation = true)
+    {
+      for (int orientation = 0; orientation < this.Orientations.Count; orientation++)
+      {
+        bool isMatch = true;
+
+        for (int i = 0; i < this.size; i++)
+        {
+          if (this.Data[i, this.size - 1] != tile.Data[i, 0])
+          {
+            isMatch = false;
+            break;
+          }
+        }
+
+        if (isMatch)
+          return true;
+
+        if (!allowReorientation)
+          break;
+
+        if (orientation < this.Orientations.Count - 1)
+          tile.NextOrientation();
+      }
+
+      return false;
+    }
+
+    private void BuildOrientations(bool[,] initialOrientation)
+    {
+      for (int i = 0; i < 4; i++)
+      {
+        if (i == 0)
+          this.cachedOrientations.Add(initialOrientation);
+        else
+          this.cachedOrientations.Add(GetRotatedTile(this.cachedOrientations[i * 3 - 3]));
+
+        this.cachedOrientations.Add(GetFlippedHorizontal(this.cachedOrientations[i * 3]));
+        this.cachedOrientations.Add(GetFlippedVertical(this.cachedOrientations[i * 3]));
+      }
+    }
+
+    private void NextOrientation()
+    {
+      this.orientation = (this.orientation + 1) % this.Orientations.Count;
+    }
+
+    private bool[,] GetRotatedTile(bool[,] tile)
+    {
+      var rotatedTile = new bool[this.size, this.size];
+
+      for (int i = 0; i < this.size; i++)
+      {
+        for (int j = 0; j < this.size; j++)
+        {
+          rotatedTile[i, j] = tile[this.size - j - 1, i];
+        }
+      }
+
+      return rotatedTile;
+    }
+
+    private bool[,] GetFlippedHorizontal(bool[,] tile)
+    {
+      var flippedTile = new bool[this.size, this.size];
+
+      for (int i = 0; i < this.size; i++)
+      {
+        for (int j = 0; j < tile.GetLength(0); j++)
+        {
+          flippedTile[i, j] = tile[this.size - i - 1, j];
+        }
+      }
+
+      return flippedTile;
+    }
+
+    private bool[,] GetFlippedVertical(bool[,] tile)
+    {
+      var flippedTile = new bool[this.size, this.size];
+
+      for (int i = 0; i < this.size; i++)
+      {
+        for (int j = 0; j < this.size; j++)
+        {
+          flippedTile[i, j] = tile[i, this.size - j - 1];
+        }
+      }
+
+      return flippedTile;
+    }
+
+    public override string ToString()
+    {
+      return this.ToString(this.orientation);
+    }
+
+    public string ToString(int orientation)
+    {
+      string output = "Tile " + this.Id + " (orientation = " + orientation + "):" + Environment.NewLine;
+
+      for (int y = 0; y < this.size; y++)
+      {
+        for (int x = 0; x < this.size; x++)
+          output += this.cachedOrientations[orientation][x, y] ? '#' : '.';
+
+        output += Environment.NewLine;
+      }
+
+      return output;
+    }
+  }
+}
