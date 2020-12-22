0c5c5d7c9ce8e4e4022d36d6d042af6749e7916f
Andrew Schenck <omnova@gmail.com>
2020-12-22 14:45:57 -0600

    Added 2020 Day 22

--- Year2020/Day22/Part1.cs
+++ Year2020/Day22/Part1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Year2020.Day22
 {
@@ -6,7 +8,23 @@ namespace AdventOfCode.Year2020.Day22
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var hands = input.Split(Environment.NewLine + Environment.NewLine).Select(s => new Queue<int>(s.Split(Environment.NewLine).Skip(1).Select(int.Parse).ToList())).ToList();
+
+      while (!hands.Any(h => h.Count == 0))
+      {
+        var play = hands.Select(h => h.Dequeue()).ToList();
+
+        int winner = play.IndexOf(play.Max());
+
+        hands[winner].Enqueue(play[winner]);
+        hands[winner].Enqueue(play[(winner + 1) % 2]);
+      }
+
+      int multiplier = 1;
+
+      long result = hands.FirstOrDefault(h => h.Count > 0).Reverse().Aggregate(0, (acc, val) => acc + (val * multiplier++));
+
+      return result;
     }
   }
 }
--- Year2020/Day22/Part2.cs
+++ Year2020/Day22/Part2.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AdventOfCode.Year2020.Day22
 {
   public class Part2 : IPuzzle
-  {
+  {    
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var hands = input.Split(Environment.NewLine + Environment.NewLine).Select(s => new Queue<byte>(s.Split(Environment.NewLine).Skip(1).Select(byte.Parse).ToList())).ToList();
+
+      byte winner = PlayGame(hands, out Queue<byte> winningHand);
+
+      int multiplier = 1;
+
+      return winningHand.Reverse().Aggregate(0, (acc, val) => acc + (val * multiplier++));
+    }
+
+    private byte PlayGame(List<Queue<byte>> hands, out Queue<byte> winningHand)
+    {
+      var handConfigurations = new List<List<string>> { new List<string>(), new List<string>() };
+
+      while (!hands.Any(h => h.Count == 0))
+      {
+        for (byte h = 0; h < hands.Count; h++)
+        {
+          if (handConfigurations[h].Contains(string.Join(',', hands[h])))
+          {
+            winningHand = hands[h];
+            return 0;
+          }
+          else
+            handConfigurations[h].Add(string.Join(',', hands[h]));
+        }
+
+        var play = hands.Select(h => h.Dequeue()).ToList();
+
+        byte? winner = null;
+
+        if (hands[0].Count >= play[0] && hands[1].Count >= play[1])
+        {
+          var subGameHands = hands.Select(h => new Queue<byte>(h.Take(play[hands.IndexOf(h)]).ToList())).ToList();
+
+          winner = PlayGame(subGameHands, out Queue<byte> dontcare);
+        }
+        else
+          winner = (play[0] > play[1] ? 0 : 1);
+
+        hands[winner.Value].Enqueue(play[winner.Value]);
+        hands[winner.Value].Enqueue(play[(winner.Value + 1) % 2]);
+      }
+
+      winningHand = hands[0].Any() ? hands[0] : hands[1];
+
+      return hands[0].Any() ? 0 : 1;
     }
   }
 }
