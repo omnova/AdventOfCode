8e7d2f9d97b4d244e183b86a445c9d7716beaa48
Andrew Schenck <omnova@gmail.com>
2020-12-23 15:43:36 -0600

    Added 2020 Day 23

--- Year2020/Day23/Part1.cs
+++ Year2020/Day23/Part1.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace AdventOfCode.Year2020.Day24
+namespace AdventOfCode.Year2020.Day23
 {
   public class Part1 : IPuzzle
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var cups = input.ToCharArray().Select(c => int.Parse(c.ToString())).ToList();
+
+      int currentCup = cups[0];
+      int totalCups = cups.Count;
+
+      for (int i = 1; i <= 100; i++)
+      {
+        int pickup1 = cups.First(c => (cups.IndexOf(c) == (cups.IndexOf(currentCup) + 1) % cups.Count));
+        int pickup2 = cups.First(c => (cups.IndexOf(c) == (cups.IndexOf(currentCup) + 2) % cups.Count));
+        int pickup3 = cups.First(c => (cups.IndexOf(c) == (cups.IndexOf(currentCup) + 3) % cups.Count));
+
+        var pickedUpCups = new List<int> { pickup1, pickup2, pickup3 };
+
+        cups.RemoveAll(c => pickedUpCups.Contains(c));
+
+        int? destinationCup = null;
+        int targetCup = currentCup - 1;
+
+        while (true)
+        {
+          if (targetCup == 0)
+            targetCup = totalCups;
+
+          if (cups.Any(c => c == targetCup))
+          {
+            destinationCup = targetCup;
+            break;
+          }
+
+          targetCup--;
+        }
+
+        cups.InsertRange(cups.IndexOf(destinationCup.Value) + 1, pickedUpCups);
+
+        currentCup = cups[(cups.IndexOf(currentCup) + 1) % cups.Count];
+      }
+
+      var result = string.Join("", cups.Skip(cups.IndexOf(1))).Replace("1", "") + string.Join("", cups.Take(cups.IndexOf(1) + 1)).Replace("1", "");
+
+      return result;
     }
   }
 }
--- Year2020/Day23/Part2.cs
+++ Year2020/Day23/Part2.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace AdventOfCode.Year2020.Day24
+namespace AdventOfCode.Year2020.Day23
 {
   public class Part2 : IPuzzle
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var rawCups = input.ToCharArray().Select(c => int.Parse(c.ToString())).ToList();
+      var cups = new LinkedList<int>();
+
+      rawCups.ForEach(c => cups.AddLast(c));
+      Enumerable.Range(cups.Max() + 1, 1000000 - cups.Count).ToList().ForEach(c => cups.AddLast(c));
+
+      var cupLookup = new Dictionary<int, LinkedListNode<int>>();
+
+      for (var cupNode = cups.First; cupNode != null; cupNode = cupNode.Next)
+      {
+        cupLookup.Add(cupNode.Value, cupNode);
+      }
+
+      int totalCups = cups.Count;
+      var currentCupNode = cups.First;      
+
+      for (int i = 1; i <= 10000000; i++)
+      {
+        var pickedUpCups = new List<LinkedListNode<int>>();
+
+        for (int p = 0; p < 3; p++)
+        {
+          var nextNode = currentCupNode.Next;
+
+          if (nextNode == null)
+            nextNode = cups.First;
+
+          pickedUpCups.Add(nextNode);
+          cups.Remove(nextNode);
+        }
+
+        int targetCupValue = currentCupNode.Value - 1;
+
+        while (true)
+        {
+          if (targetCupValue == 0)
+            targetCupValue = totalCups;
+
+          if (!pickedUpCups.Any(c => c.Value == targetCupValue))
+            break;
+
+          targetCupValue--;
+        }
+
+        var destinationNode = cupLookup[targetCupValue];
+
+        pickedUpCups.Reverse();
+        pickedUpCups.ForEach(p => cups.AddAfter(destinationNode, p));
+
+        currentCupNode = currentCupNode.Next != null ? currentCupNode.Next : cups.First;
+      }
+
+      long result = (long)cupLookup[1].Next.Value * (long)cupLookup[1].Next.Next.Value;
+
+      return result; 
     }
   }
 }
