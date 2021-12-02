a4f0687f40fba874e6e99ef58a5525efff01837d
Andrew Schenck <omnova@gmail.com>
2021-12-02 15:07:39 -0600

    Added 2021 day 2

--- Year2021/Day02/Part1.cs
+++ Year2021/Day02/Part1.cs
@@ -7,7 +7,22 @@ namespace AdventOfCode.Year2021.Day02
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var commands = input.Split(Environment.NewLine).Select(c => c.Split(' ')).ToList();
+
+      int x = 0;
+      int z = 0;
+
+      foreach (var command in commands)
+      {
+        if (command[0] == "forward")
+          x += int.Parse(command[1]);
+        else if (command[0] == "down")
+          z += int.Parse(command[1]);
+        else if (command[0] == "up")
+          z -= int.Parse(command[1]);
+      }
+
+      return x * z;
     }
   }
 }
--- Year2021/Day02/Part2.cs
+++ Year2021/Day02/Part2.cs
@@ -7,7 +7,30 @@ namespace AdventOfCode.Year2021.Day02
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var commands = input.Split(Environment.NewLine).Select(c => c.Split(' ')).ToList();
+
+      int x = 0;
+      int z = 0;
+      int aim = 0;
+
+      foreach (var command in commands)
+      {
+        string instruction = command[0];
+        int value = int.Parse(command[1]);
+
+        if (instruction == "forward")
+        {
+          x += value;
+
+          z += (aim * value);
+        }
+        else if (instruction == "down")
+          aim += value;
+        else if (instruction == "up")
+          aim -= value;
+      }
+
+      return x * z;
     }
   }
 }
