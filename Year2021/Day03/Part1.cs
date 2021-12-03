b146d8578822101f7b24bc69d6c424e66748ba85
Andrew Schenck <omnova@gmail.com>
2021-12-03 00:53:36 -0600

    Added 2021 day 3

--- Year2021/Day03/Part1.cs
+++ Year2021/Day03/Part1.cs
@@ -7,7 +7,25 @@ namespace AdventOfCode.Year2021.Day03
   {
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var numbers = input.Split(Environment.NewLine);
+
+      var bitCounts = new int[numbers[0].Length];
+
+      foreach (var number in numbers)
+      {
+        for (int i = 0; i < number.Length; i++)
+        {
+          if (number[i] == '1')
+            bitCounts[i]++;
+          else
+            bitCounts[i]--;
+        }
+      }
+
+      var gammaRate = Convert.ToInt32(string.Join("", bitCounts.Select(c => Math.Min(Math.Max(c, 0), 1)).ToArray()), 2);
+      var epsilonRate = Convert.ToInt32(string.Join("", bitCounts.Select(c => Math.Min(Math.Max(-c, 0), 1)).ToArray()), 2);
+      
+      return gammaRate * epsilonRate;
     }
   }
 }
--- Year2021/Day03/Part2.cs
+++ Year2021/Day03/Part2.cs
@@ -8,7 +8,43 @@ namespace AdventOfCode.Year2021.Day03
   {   
     public object Run(string input)
     {
-      throw new NotImplementedException();
+      var numbers = input.Split(Environment.NewLine);
+
+      var oxygenGeneratorNumbers = numbers.ToArray();
+      var co2ScrubberNumbers = numbers.ToArray();
+
+      for (int i = 0; i < numbers[0].Length; i++)
+      {
+        var oxygenGeneratorNumbers0 = oxygenGeneratorNumbers.Where(n => n[i] == '0').ToArray();
+        var oxygenGeneratorNumbers1 = oxygenGeneratorNumbers.Where(n => n[i] == '1').ToArray();
+
+        if (oxygenGeneratorNumbers1.Length >= oxygenGeneratorNumbers0.Length)
+          oxygenGeneratorNumbers = oxygenGeneratorNumbers1;
+        else
+          oxygenGeneratorNumbers = oxygenGeneratorNumbers0;
+
+        if (oxygenGeneratorNumbers.Length == 1)
+          break;
+      }
+
+      for (int i = 0; i < numbers[0].Length; i++)
+      {
+        var co2ScrubberNumbers0 = co2ScrubberNumbers.Where(n => n[i] == '0').ToArray();
+        var co2ScrubberNumbers1 = co2ScrubberNumbers.Where(n => n[i] == '1').ToArray();
+
+        if (co2ScrubberNumbers0.Length <= co2ScrubberNumbers1.Length)
+          co2ScrubberNumbers = co2ScrubberNumbers0;
+        else
+          co2ScrubberNumbers = co2ScrubberNumbers1;
+
+        if (co2ScrubberNumbers.Length == 1)
+          break;
+      }
+
+      int oxygenGeneratorRating = Convert.ToInt32(oxygenGeneratorNumbers[0], 2);
+      int co2ScrubberRating = Convert.ToInt32(co2ScrubberNumbers[0], 2);
+
+      return oxygenGeneratorRating * co2ScrubberRating;
     }
   }
 }
