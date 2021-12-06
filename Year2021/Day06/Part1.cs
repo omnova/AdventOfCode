e47c275c9125c400af9c9e995b18d0f4c711f388
Andrew Schenck <omnova@gmail.com>
2021-12-05 23:49:17 -0600

    Added 2021 day 6

---
+++ Year2021/Day06/Part1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace AdventOfCode.Year2021.Day06
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var fish = input.Split(',').Select(int.Parse).ToList();
+
+      for (int i = 0; i < 80; i++)
+      {
+        int newFish = 0;
+
+        for (int f = 0; f < fish.Count; f++)
+        {
+          fish[f]--;
+
+          if (fish[f] < 0)
+          {
+            fish[f] = 6;
+            newFish++;
+          }
+        }
+
+        for (int f = 0; f < newFish; f++)
+          fish.Add(8);
+      }     
+
+      return fish.Count;
+    }
+  }
+}
---
+++ Year2021/Day06/Part2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Day06
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var fishes = input.Split(',').Select(int.Parse).ToList();
+
+      var fishCounts = new long[9];
+
+      foreach (var fish in fishes)
+        fishCounts[fish]++;
+
+      for (long i = 0; i < 256; i++)
+      {
+        long newFish = fishCounts[0];
+
+        for (int f = 1; f < fishCounts.Length; f++)
+        {
+          fishCounts[f - 1] = fishCounts[f];
+        }
+
+        fishCounts[6] += newFish;
+        fishCounts[8] = newFish;
+      }
+
+      return fishCounts.Sum();
+    }
+  }
+}
