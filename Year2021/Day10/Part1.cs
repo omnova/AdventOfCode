bc85ea929e8ebd2322ebb78531ff3d2c766c4e7e
Andrew Schenck <omnova@gmail.com>
2021-12-10 13:13:32 -0600

    Added 2021 day 10

---
+++ Year2021/Day10/Part1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace AdventOfCode.Year2021.Day10
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var lines = input.Split(Environment.NewLine);
+
+      var openCloseMatches = new Dictionary<char, char>
+      {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+        { '<', '>' }
+      };
+
+      var corruptScores = new Dictionary<char, int>
+      {
+        { ')', 3 },
+        { ']', 57 },
+        { '}', 1197 },
+        { '>', 25137 },
+      };
+
+      int score = 0;
+
+      foreach (string line in lines)
+      {
+        var stack = new Stack<char>();
+
+        foreach (char c in line)
+        {
+          if (openCloseMatches.ContainsKey(c))
+            stack.Push(c);
+          else if (c == openCloseMatches[stack.Peek()])
+            stack.Pop();
+          else
+          {
+            score += corruptScores[c];
+            break;
+          }
+        }
+      }
+
+      return score;
+    }
+  }
+}
---
+++ Year2021/Day10/Part2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Day10
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var lines = input.Split(Environment.NewLine).ToList();
+
+      var openCloseMatches = new Dictionary<char, char>
+      {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' },
+        { '<', '>' }
+      };
+
+      var incompleteScoreMap = new Dictionary<char, int>
+      {
+        { '(', 1 },
+        { '[', 2 },
+        { '{', 3 },
+        { '<', 4 },
+      };
+
+      var incompleteScores = new List<long>();
+
+      foreach (string line in lines)
+      {
+        var stack = new Stack<char>();
+
+        bool isCorrupt = false;
+
+        foreach (char c in line)
+        {
+          if (openCloseMatches.ContainsKey(c))
+            stack.Push(c);
+          else if (c == openCloseMatches[stack.Peek()])
+            stack.Pop();
+          else
+          {
+            isCorrupt = true;
+            break;
+          }
+        }
+
+        if (!isCorrupt && stack.Count > 0)
+        {
+          long score = 0;
+
+          while (stack.TryPop(out char missingChar))
+          {
+            score = score * (long)5 + incompleteScoreMap[missingChar];
+          }
+
+          incompleteScores.Add(score);
+        }
+      }
+
+      var middleScore = incompleteScores.OrderBy(s => s).Skip(incompleteScores.Count / 2).FirstOrDefault();
+
+      return middleScore;
+    }
+  }
+}
