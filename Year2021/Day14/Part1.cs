164f101e365c7ce1fbb3695931e18593a399ad11
Andrew Schenck <omnova@gmail.com>
2021-12-13 00:42:56 -0600

    Prep for day 14

---
+++ Year2021/Day14/Part1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Day14
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+
+
+      return null;
+    }
+  }
+}
---
+++ Year2021/Day14/Part2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Day14
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      return null;
+    }
+  }
+}
