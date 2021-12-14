79483c7b54bff96defeceb3b2b2c76402f342148
Andrew Schenck <omnova@gmail.com>
2021-12-14 01:17:22 -0600

    Added 2021 day 14

--- Year2021/Day14/Part1.cs
+++ Year2021/Day14/Part1.cs
@@ -8,9 +8,30 @@ namespace AdventOfCode.Year2021.Day14
   {
     public object Run(string input)
     {
+      string template = input.Split(Environment.NewLine)[0];
+      var insertionRules = input.Split(Environment.NewLine).Skip(2).Select(l => l.Split(" -> ")).ToDictionary(l => l[0], l => l[1]);
 
+      for (int i = 0; i < 10; i++)
+      {
+        string nextTemplate = template;
 
-      return null;
+        for (int c = template.Length - 1; c > 0; c--)
+        {
+          string pair = template[c - 1].ToString() + template[c].ToString();
+
+          if (insertionRules.TryGetValue(pair, out string insertion))
+            nextTemplate = nextTemplate.Insert(c, insertion);
+        }
+
+        template = nextTemplate;
+      }
+
+      var charCounts = template.ToCharArray().Distinct().Select(c => template.ToCharArray().Count(l => l == c)).ToList();
+
+      int minCount = charCounts.Min();
+      int maxCount = charCounts.Max();
+
+      return maxCount - minCount;
     }
   }
 }
--- Year2021/Day14/Part2.cs
+++ Year2021/Day14/Part2.cs
@@ -8,7 +8,50 @@ namespace AdventOfCode.Year2021.Day14
   {
     public object Run(string input)
     {
-      return null;
+      string template = input.Split(Environment.NewLine)[0];
+      var insertionRules = input.Split(Environment.NewLine).Skip(2).Select(l => l.Split(" -> ")).ToDictionary(l => l[0], l => l[1]);
+
+      var pairCounts = new Dictionary<string, long>();
+      var charCounts = template.ToCharArray().Distinct().ToDictionary(c => c, c => (long)(template.ToCharArray().Count(l => l == c)));
+
+      for (int c = 0; c < template.Length - 1; c++)
+      {
+        string pair = template[c].ToString() + template[c + 1].ToString();
+
+        if (!pairCounts.TryAdd(pair, 1))
+          pairCounts[pair]++;
+      }
+
+      for (int i = 0; i < 40; i++)
+      {
+        var newPairCounts = new Dictionary<string, long>();
+
+        foreach (var pair in pairCounts)
+        {
+          if (insertionRules.TryGetValue(pair.Key, out string insertion))
+          {
+            string firstNewPair = pair.Key[0] + insertion;
+
+            if (!newPairCounts.TryAdd(firstNewPair, pair.Value))
+              newPairCounts[firstNewPair] += pair.Value;
+
+            string secondNewPair = insertion + pair.Key[1];
+
+            if (!newPairCounts.TryAdd(secondNewPair, pair.Value))
+              newPairCounts[secondNewPair] += pair.Value;          
+
+            if (!charCounts.TryAdd(insertion[0], pair.Value))
+              charCounts[insertion[0]] += pair.Value;          
+          }
+        }
+
+        pairCounts = newPairCounts;
+      }
+
+      long minCount = charCounts.Values.Min();
+      long maxCount = charCounts.Values.Max();
+
+      return maxCount - minCount;
     }
   }
 }
