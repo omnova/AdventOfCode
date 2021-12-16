26dd37111595a98c26b7c32630e37102ba08b8d4
Andrew Schenck <omnova@gmail.com>
2021-12-16 01:55:42 -0600

    Added 2021 day 16

---
+++ Year2021/Day16/Part1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Year2021.Day16
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      string packet = string.Join(null, input.Select(c => Convert.ToByte("0" + c.ToString(), 16)).Select(b => Convert.ToString(b, 2).PadLeft(8, '0').Substring(4)).ToArray());
+
+      int versionNumberTotal = 0;
+      int cursor = 0;
+
+      ProcessPacket(packet, ref cursor, ref versionNumberTotal);
+
+      return versionNumberTotal;
+    }
+
+    private void ProcessPacket(string packet, ref int cursor, ref int versionTotal)
+    {
+      int packetVersion = Convert.ToInt32(packet.Substring(cursor, 3), 2);
+      int packetTypeID = Convert.ToInt32(packet.Substring(cursor + 3, 3), 2);
+
+      versionTotal += packetVersion;
+
+      cursor += 6;
+
+      if (packetTypeID == 4)
+        ProcessLiteralPacket(packet, ref cursor);
+      else
+        ProcessOperatorPacket(packet, ref cursor, ref versionTotal);
+    }
+
+    private void ProcessLiteralPacket(string packet, ref int cursor)
+    {
+      while (true)
+      {
+        cursor += 5;
+
+        if (packet[cursor - 5] == '0')
+          break;
+      }
+    }
+
+    private void ProcessOperatorPacket(string packet, ref int cursor, ref int versionTotal)
+    {
+      char lengthMode = packet[cursor++];
+
+      if (lengthMode == '0')
+      {
+        int length = Convert.ToInt32(packet.Substring(cursor, 15), 2);
+
+        cursor += 15;
+
+        int subPacketEnd = cursor + length;
+
+        while (cursor < subPacketEnd)
+        {
+          ProcessPacket(packet, ref cursor, ref versionTotal);
+        }
+      }
+      else
+      {
+        int numSubPackets = Convert.ToInt32(packet.Substring(cursor, 11), 2);
+
+        cursor += 11;
+
+        for (int i = 0; i < numSubPackets; i++)
+        {
+          ProcessPacket(packet, ref cursor, ref versionTotal);
+        }
+      }
+    }
+  }
+}
---
+++ Year2021/Day16/Part2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Year2021.Day16
+{
+  public class Part2 : IPuzzle
+  {  
+    public object Run(string input)
+    {
+      string packet = string.Join(null, input.Select(c => Convert.ToByte("0" + c.ToString(), 16)).Select(b => Convert.ToString(b, 2).PadLeft(8, '0').Substring(4)).ToArray());
+
+      int cursor = 0;
+
+      long total = GetPacketValue(packet, ref cursor);
+
+      return total;
+    }
+
+    private long GetPacketValue(string packet, ref int cursor)
+    {      
+      int packetTypeID = Convert.ToInt32(packet.Substring(cursor + 3, 3), 2);
+
+      cursor += 6;
+
+      if (packetTypeID == 4)
+        return GetLiteralPacketValue(packet, ref cursor);
+      else
+        return GetOperatorPacketValue(packet, ref cursor, packetTypeID);
+    }
+
+    private long GetLiteralPacketValue(string packet, ref int cursor)
+    {
+      string literal = string.Empty;
+
+      while (cursor < packet.Length)
+      {
+        literal += packet.Substring(cursor + 1, 4);
+
+        cursor += 5;
+
+        if (packet[cursor - 5] == '0')
+          break;
+      }
+
+      return Convert.ToInt64(literal, 2);
+    }
+
+    private long GetOperatorPacketValue(string packet, ref int cursor, int packetTypeID)
+    {
+      char lengthMode = packet[cursor++];
+
+      var packetValues = new List<long>();
+
+      if (lengthMode == '0')
+      {
+        int length = Convert.ToInt32(packet.Substring(cursor, 15), 2);
+
+        cursor += 15;
+
+        int subPacketEnd = cursor + length;
+
+        while (cursor < subPacketEnd)
+        {
+          long value = GetPacketValue(packet, ref cursor);
+
+          packetValues.Add(value);
+        }
+      }
+      else
+      {
+        int numSubPackets = Convert.ToInt32(packet.Substring(cursor, 11), 2);
+
+        cursor += 11;
+
+        for (int i = 0; i < numSubPackets; i++)
+        {
+          long value = GetPacketValue(packet, ref cursor);
+
+          packetValues.Add(value);
+        }
+      }
+
+      if (packetTypeID == 0)
+        return packetValues.Sum();
+      else if (packetTypeID == 1)
+        return packetValues.Aggregate((a, b) => (long)a * (long)b);
+      else if (packetTypeID == 2)
+        return packetValues.Min();
+      else if (packetTypeID == 3)
+        return packetValues.Max();
+      else if (packetTypeID == 5)
+        return packetValues[0] > packetValues[1] ? 1 : 0;
+      else if (packetTypeID == 6)
+        return packetValues[0] < packetValues[1] ? 1 : 0;
+      else if (packetTypeID == 7)
+        return packetValues[0] == packetValues[1] ? 1 : 0;
+
+      throw new Exception("wat");
+    }
+  }
+}
