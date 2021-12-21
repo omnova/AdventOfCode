68035904dbf7fb6b7b156665b99ab5c7b913bce6
Andrew Schenck <omnova@gmail.com>
2021-12-21 00:15:10 -0600

    Added 2021 day 21

---
+++ Year2021/Day21/Part1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Day21
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var playerPositions = input.Split(Environment.NewLine).Select(l => int.Parse(l.Split(": ").Last())).ToArray();
+      var playerScores = new int[2];
+
+      int numRolls = 0;
+
+      while (true)
+      {
+        for (int p = 0; p < playerPositions.Length; p++)
+        {
+          int roll = ++numRolls + ++numRolls + ++numRolls;
+
+          playerPositions[p] = ((playerPositions[p] - 1 + roll) % 10) + 1;
+          playerScores[p] += playerPositions[p];
+
+          if (playerScores[p] >= 1000)
+            return playerScores[(p + 1) % 2] * numRolls;
+        }
+      }
+    }
+  }
+}
---
+++ Year2021/Day21/Part2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2021.Day21
+{
+  public class Part2 : IPuzzle
+  {  
+    public object Run(string input)
+    {
+      var playerPositions = input.Split(Environment.NewLine).Select(l => int.Parse(l.Split(": ").Last())).ToArray();
+      var playerScores = new int[2];
+
+      int numRolls = 0;
+
+      // Track counts of games at each stage (should be much more finite than number of universes
+      // There's probably math to figure this out
+
+      while (true)
+      {
+        for (int p = 0; p < playerPositions.Length; p++)
+        {
+          int roll = ++numRolls + ++numRolls + ++numRolls;
+
+          playerPositions[p] = ((playerPositions[p] - 1 + roll) % 10) + 1;
+          playerScores[p] += playerPositions[p];
+
+          if (playerScores[p] >= 1000)
+            return playerScores[(p + 1) % 2] * numRolls;
+        }
+      }
+    }
+  }
+}
