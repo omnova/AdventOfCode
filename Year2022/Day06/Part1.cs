33294b222c97c08962e4b9f6a9a695576b88441b
Andrew Schenck <omnova@gmail.com>
2022-12-06 00:42:06 -0700

    Added 2022 Day 6

---
+++ Year2022/Day06/Part1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day06
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int i = 0;
+      var buffer = new char[4];
+
+      while (i < 3 || buffer.Distinct().Count() != 4)
+      {
+        buffer[i++ % 4] = input[i];
+      }
+
+      return i + 1;
+    }
+  }
+}
---
+++ Year2022/Day06/Part2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day06
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int i = 0;
+      var buffer = new char[14];
+
+      while (i < 13 || buffer.Distinct().Count() != 14)
+      {
+        buffer[i++ % 14] = input[i];
+      }
+
+      return i + 1;
+    }
+  }
+}
