79a8a3bf8955aa9e3a0cabaab9ae44b836cf3ffd
Andrew Schenck <omnova@gmail.com>
2022-12-06 00:41:14 -0700

    Added 2022 Day 4

---
+++ Year2022/Day04/Part1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day04
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int numEncompassingPairs = input
+        .Split(Environment.NewLine)
+        .Select(l => l.Split(",").Select(r => r.Split("-").Select(int.Parse).ToArray()).ToArray())
+        .Where(l => (l[0][0] <= l[1][0] && l[0][1] >= l[1][1]) || (l[1][0] <= l[0][0] && l[1][1] >= l[0][1]))
+        .Count();
+
+      return numEncompassingPairs;
+    }
+  }
+}
