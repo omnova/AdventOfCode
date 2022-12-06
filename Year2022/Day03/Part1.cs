776c79782fa8735191db7791b3d4e098e7ea06ac
Andrew Schenck <omnova@gmail.com>
2022-12-06 00:40:48 -0700

    Added 2022 Day 3

---
+++ Year2022/Day03/Part1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day03
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int totalPriority = input
+        .Split(Environment.NewLine)
+        .Select(l => new HashSet<char>[] { new HashSet<char>(l.Substring(0, l.Length / 2)), new HashSet<char>(l.Substring(l.Length / 2)) })        
+        .Select(l => l[0].Intersect(l[1]).First())
+        .Select(l => l <= 'Z' ? l - 'A' + 27 : l - 'a' + 1)
+        .Sum();
+
+      return totalPriority;
+    }
+  }
+}
---
+++ Year2022/Day03/Part2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day03
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int totalBadgePriorities = input
+        .Split(Environment.NewLine)
+        .Select((l, i) => new { l, i })
+        .GroupBy(x => x.i / 3)
+        .Select(g => g.Select(r => new HashSet<char>(r.l)).ToArray())
+        .Select(g => g[0].Intersect(g[1]).Intersect(g[2]).First())
+        .Select(l => l <= 'Z' ? l - 'A' + 27 : l - 'a' + 1)
+        .Sum();
+
+      return totalBadgePriorities;
+    }
+  }
+}
