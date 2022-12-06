33804aac28f8dbcd18606db559842208bcc10cca
Andrew Schenck <omnova@gmail.com>
2022-12-06 00:40:11 -0700

    Added 2022 Day 1

---
+++ Year2022/Day01/Part1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day01
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int maxTotal = input
+        .Split(Environment.NewLine + Environment.NewLine)
+        .Select(e => e.Split(Environment.NewLine).Select(int.Parse).Sum())
+        .Max();
+
+      return maxTotal;
+    }
+  }
+}
---
+++ Year2022/Day01/Part2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day01
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int top3Total = input
+        .Split(Environment.NewLine + Environment.NewLine)
+        .Select(e => e.Split(Environment.NewLine).Select(int.Parse).Sum())
+        .OrderByDescending(e => e)
+        .Take(3)
+        .Sum();
+
+      return top3Total;
+    }
+  }
+}
