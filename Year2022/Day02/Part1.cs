5c5ccdd879940839198c4be0e52928df44ced7c0
Andrew Schenck <omnova@gmail.com>
2022-12-06 00:40:36 -0700

    Added 2022 Day 2

---
+++ Year2022/Day02/Part1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day02
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var score = input
+        .Split(Environment.NewLine)
+        .Select(l => new int[] { Convert.ToInt32(l[0]) - (byte)'A', Convert.ToInt32(l[2]) - (byte)'X' })
+        .Select(l => (l[1] + 1) + ((l[1] - l[0] + 4) % 3) * 3)
+        .Sum();
+
+      return score;
+    }
+  }
+}
---
+++ Year2022/Day02/Part2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day02
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      int score = input
+        .Split(Environment.NewLine)
+        .Select(l => new int[] { Convert.ToInt32(l[0]) - (byte)'A', Convert.ToInt32(l[2]) - (byte)'X' })
+        .Select(l => ((l[0] + l[1] + 2) % 3) + 1 + (l[1] * 3))
+        .Sum();
+
+      return score;
+    }
+  }
+}
