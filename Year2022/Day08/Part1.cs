bad35d34b60b99fb13a65672af714b81d5812ca1
Andrew Schenck <omnova@gmail.com>
2022-12-10 14:24:43 -0700

    Added 2022 Day 8

---
+++ Year2022/Day08/Part1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day08
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var grid = input.Split(Environment.NewLine).Select(l => l.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray()).ToArray();
+
+      var treeVisiblity = new int[grid[0].Length, grid.Length];
+
+      for (int i = 0; i < grid.Length; i++)
+      {
+        int currentheight = -1;
+
+        for (int j = 0; j < grid[0].Length; j++)
+        {
+          if (grid[i][j] > currentheight)
+          {
+            currentheight = grid[i][j];
+            treeVisiblity[j, i] = 1;
+          }
+        }
+
+        currentheight = -1;
+
+        for (int j = grid[0].Length - 1; j >= 0; j--)
+        {
+          if (grid[i][j] > currentheight)
+          {
+            currentheight = grid[i][j];
+            treeVisiblity[j, i] = 1;
+          }
+        }
+      }
+
+      for (int j = 0; j < grid[0].Length; j++)
+      {
+        int currentheight = -1;
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+          if (grid[i][j] > currentheight)
+          {
+            currentheight = grid[i][j];
+            treeVisiblity[j, i] = 1;
+          }
+        }
+
+        currentheight = -1;
+
+        for (int i = grid.Length - 1; i >= 0; i--)
+        {
+          if (grid[i][j] > currentheight)
+          {
+            currentheight = grid[i][j];
+            treeVisiblity[j, i] = 1;
+          }
+        }
+      }
+
+      int visibleTrees = treeVisiblity.ToArray().Sum();
+
+      return visibleTrees;
+    }
+  }
+}
---
+++ Year2022/Day08/Part2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day08
+{
+  public class Part2 : IPuzzle
+  {   
+    public object Run(string input)
+    {
+      var grid = input.Split(Environment.NewLine).Select(l => l.ToCharArray().Select(c => int.Parse(c.ToString())).ToArray()).ToArray();
+
+      int maxScenicScore = 0;
+
+      for (int i = 1; i < grid.Length -1; i++)
+      {
+        for (int j = 1; j < grid[0].Length - 1; j++)
+        {
+          int height = grid[i][j];
+          var scenicScoreComponents = new int[4];
+
+          for (int x = i + 1; x < grid.Length; x++)
+          {
+            scenicScoreComponents[0]++;
+
+            if (grid[x][j] >= height)
+              break;
+          }
+
+          for (int x = i - 1; x >= 0; x--)
+          {
+            scenicScoreComponents[1]++;
+
+            if (grid[x][j] >= height)
+              break;
+          }
+
+          for (int y = j + 1; y < grid[0].Length; y++)
+          {
+            scenicScoreComponents[2]++;
+
+            if (grid[i][y] >= height)
+              break;
+          }
+
+          for (int y = j - 1; y >= 0; y--)
+          {
+            scenicScoreComponents[3]++;
+
+            if (grid[i][y] >= height)
+              break;
+          }
+
+          int scenicScore = scenicScoreComponents.Aggregate(1, (acc, val) => acc * val);
+
+          maxScenicScore = Math.Max(scenicScore, maxScenicScore);
+        }       
+      }
+
+      return maxScenicScore;
+    }
+  }
+}
