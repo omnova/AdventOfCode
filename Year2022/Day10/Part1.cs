18edb1441fc7962a1ff3ecc065cfb1b531695a7d
Andrew Schenck <omnova@gmail.com>
2022-12-10 14:24:12 -0700

    Added 2022 Day 10

---
+++ Year2022/Day10/Part1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day10
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var instructions = input.Split(Environment.NewLine).Select(l => l.Split(' ').ToArray());
+      var importantCycles = new int[] { 20, 60, 100, 140, 180, 220 };
+
+      int x = 1;
+      int total = 0;
+      int cycle = 1;
+
+      foreach (var instruction in instructions)
+      {
+        if (importantCycles.Contains(cycle))
+          total += (cycle * x);
+
+        if (instruction.Length > 1)
+        {
+          if (importantCycles.Contains(++cycle))
+            total += (cycle * x);
+
+          x += int.Parse(instruction[1]);
+        }
+
+        cycle++;
+      }
+
+      return total;
+    }
+  }
+}
---
+++ Year2022/Day10/Part2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day10
+{
+  public class Part2 : IPuzzle
+  {   
+    public object Run(string input)
+    {
+      var instructions = input.Split(Environment.NewLine).Select(l => l.Split(' ').ToArray());
+
+      var screen = new bool[40, 6];
+
+      int x = 1;
+      int cycle = 1;
+
+      foreach (var instruction in instructions)
+      {
+        screen[(cycle - 1) % 40, (cycle - 1) / 40] = (((cycle - 1) % 40 >= x - 1) && ((cycle - 1) % 40 <= x + 1));
+
+        if (instruction.Length > 1)
+        {
+          cycle++;
+          screen[(cycle - 1) % 40, (cycle - 1) / 40] = (((cycle - 1) % 40 >= x - 1) && ((cycle - 1) % 40 <= x + 1));
+
+          x += int.Parse(instruction[1]);
+        }
+
+        cycle++;
+      }
+
+      return Environment.NewLine + screen.ToString(c => c ? "#" : ".");
+    }
+  }
+}
