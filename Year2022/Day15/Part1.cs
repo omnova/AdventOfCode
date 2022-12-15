24f0d46b24632c3f5deae530bfa255632ecce3ad
Andrew Schenck <omnova@gmail.com>
2022-12-15 00:22:25 -0700

    Added 2022 Day 15

---
+++ Year2022/Day15/Part1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace AdventOfCode.Year2022.Day15
+{
+  public class Part1 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var beacons = input
+        .Split(Environment.NewLine)
+        .Select(l => new int[]
+        {
+          int.Parse(l.Substring(l.IndexOf('=') + 1, l.IndexOf(',') - l.IndexOf('=') - 1)),
+          int.Parse(l.Substring(l.IndexOf("y=") + 2, l.IndexOf(':') - l.IndexOf("y=") - 2)),
+          int.Parse(l.Substring(l.LastIndexOf("x=") + 2, l.LastIndexOf(',') - l.LastIndexOf("x=") - 2)),
+          int.Parse(l.Substring(l.LastIndexOf('=') + 1))
+        })      
+       .ToArray();
+
+      int y = 2000000;
+
+      int maxX = beacons.Select(b => Math.Max(b[0], b[2])).Max() * 2;
+
+      var line = new char[maxX * 2 + 1];
+
+      foreach (var beacon in beacons)
+      {
+        if (beacon[1] == y)
+          line[beacon[0] + maxX] = 'S';
+
+        if (beacon[3] == y)
+          line[beacon[2] + maxX] = 'B';
+
+        int stepsX = Math.Abs(beacon[2] - beacon[0]);
+        int stepsY = Math.Abs(beacon[3] - beacon[1]);
+
+        int radius = stepsX + stepsY;
+        int effectiveRadius = Math.Max(0, radius - Math.Abs(y - beacon[1]));       
+
+        if (effectiveRadius > 0)
+        {
+          for (int x = beacon[0] - effectiveRadius; x <= beacon[0] + effectiveRadius; x++)
+          {
+            if (line[x + maxX] == 0)
+              line[x + maxX] = '#';
+          }
+        }
+      }
+
+      return line.Where(x => x == '#').Count();
+    }
+  }
+}
---
+++ Year2022/Day15/Part2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Year2022.Day15
+{
+  public class Part2 : IPuzzle
+  {
+    public object Run(string input)
+    {
+      var beacons = input
+        .Split(Environment.NewLine)
+        .Select(l => new List<int>
+        {
+          int.Parse(l.Substring(l.IndexOf('=') + 1, l.IndexOf(',') - l.IndexOf('=') - 1)),
+          int.Parse(l.Substring(l.IndexOf("y=") + 2, l.IndexOf(':') - l.IndexOf("y=") - 2)),
+          int.Parse(l.Substring(l.LastIndexOf("x=") + 2, l.LastIndexOf(',') - l.LastIndexOf("x=") - 2)),
+          int.Parse(l.Substring(l.LastIndexOf('=') + 1))
+        })
+        .Select(l => new int[] { l[0], l[1], l[2], l[3], Math.Abs(l[2] - l[0]) + Math.Abs(l[3] - l[1]) })
+        .ToArray();
+
+      int maxCoord = 4000000;
+
+      foreach (var beacon in beacons)
+      { 
+        int searchRadius = beacon[4] + 1;
+
+        for (int x = beacon[0] - searchRadius; x <= beacon[0] + searchRadius; x++)
+        {
+          if (x < 0 || x > maxCoord)
+            continue;
+
+          int y = beacon[1] - (searchRadius - Math.Abs(beacon[0] - x));
+
+          if (y >= 0)
+          {
+            bool isBeacon = IsMissingBeacon(beacon, x, y, beacons);
+
+            if (isBeacon)
+            {
+              long frequency = ((long)x * 4000000) + (long)y;
+
+              return frequency;
+            }
+          }
+
+          y = beacon[1] + (searchRadius - Math.Abs(beacon[0] - x));
+
+          if (y <= maxCoord)
+          {
+            bool isBeacon = IsMissingBeacon(beacon, x, y, beacons);
+
+            if (isBeacon)
+            {
+              long frequency = ((long)x * 4000000) + (long)y;
+
+              return frequency;
+            }
+          }
+        }
+      }
+
+      return null;
+    }
+
+    private bool IsMissingBeacon(int[] beacon, int x, int y, int[][] beacons)
+    {
+      foreach (var otherBeacon in beacons)
+      {
+        if (beacon == otherBeacon)
+          continue;
+
+        if ((otherBeacon[0] == x && otherBeacon[1] == y) || (otherBeacon[2] == x && otherBeacon[3] == y))
+          return false;
+
+        int radius = otherBeacon[4];
+        int stairDistance = Math.Abs(x - otherBeacon[0]) + Math.Abs(y - otherBeacon[1]);
+
+        if (stairDistance <= radius)
+          return false;
+      }
+
+      return true;
+    }
+  }
+}
